ef75833b123135944bb8bcb0e91ebf5b7a0ff56f
Ken <ken.aleksander@gmail.com>
2022-10-09 20:40:33 +0200

    :art: Scanning react

--- generator/walk-ts.ts
+++ generator/walk-ts.ts
@@ -1,25 +1,40 @@
 import path from "path";
 import scanner from "react-scanner";
 
+const config = {
+  rootDir: ".",
+  crawlFrom: "repos",
+  globs: ["**/!(*.test|*.spec|*.stories|*.story).@(jsx|tsx)"],
+  exclude: (dirname: string) => dirname === "node_modules",
+  getComponentName: ({
+    imported,
+    moduleName,
+  }: {
+    imported: string;
+    moduleName: string;
+  }) => imported || path.basename(moduleName),
+  includeSubComponents: true,
+};
+
 export const walkTs = async () => {
   await scanner.run({
-    rootDir: ".",
-    crawlFrom: "repos",
-    globs: ["**/!(*.test|*.spec|*.stories|*.story).@(jsx|tsx)"],
-    exclude: (dirname: string) => dirname === "node_modules",
-    getComponentName: ({
-      imported,
-      moduleName,
-    }: {
-      imported: string;
-      moduleName: string;
-    }) => imported || path.basename(moduleName),
-    includeSubComponents: true,
+    ...config,
     processors: [
-      // Count only component, not props, and save the result to a file
-      ["count-components-and-props", { outputTo: "out/components.json" }],
+      ["count-components-and-props", { outputTo: "out/tags.json" }],
       ["raw-report", { outputTo: "out/raw.json" }],
     ],
+  });
+  await scanner.run({
+    ...config,
+    importedFrom: /@navikt\/ds-react/,
+    processors: [
+      ["count-components-and-props", { outputTo: "out/components.json" }],
+    ],
+  });
+  await scanner.run({
+    ...config,
+    importedFrom: /@navikt\/ds-icons/,
+    processors: [["count-components", { outputTo: "out/icons.json" }]],
   });
 
   /*   data.forEach((x) => {
