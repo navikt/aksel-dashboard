016b81aa73f9ca1de64893ea59c8def2732e09d8
Ken <ken.aleksander@gmail.com>
2022-10-11 20:30:40 +0200

    not used icons

--- dashboard/pages/ikoner/index.tsx
+++ dashboard/pages/ikoner/index.tsx
@@ -4,8 +4,9 @@ import useSWR from "swr";
 import { NeutralBar } from "../../comps/Bar";
 import { Layout } from "../../comps/Layout";
 import { fetcher } from "../../lib/fetcher";
+import ICONS from "@navikt/ds-icons/meta.json";
 
-const Eksempel: NextPage = () => {
+const Eksempel: NextPage = ({ icons }: { icons: string[] }) => {
   const { data, error } = useSWR(`/api/summary/ikoner`, fetcher);
 
   if (error) return <Layout>failed to load </Layout>;
@@ -18,31 +19,56 @@ const Eksempel: NextPage = () => {
     a.props > b.props ? -1 : 1
   )[0]?.props;
 
+  const notUsed = icons.filter((x) => !data.find((y) => y.name === x));
+
   return (
     <Layout>
-      <div className="grid gap-2">
-        {data.map((x) => {
-          return (
-            <Link key={x.name} href={`/ikoner/${x.name}`} passHref>
-              <a className="grid gap-12 grid-cols-3 p-3 bg-gray-800 rounded hover:bg-gray-700">
-                <span className="text-lg ">{`<${x.name}>`}</span>
-                <div className="grid gap-1">
-                  <span className="text-sm text-gray-400">{`${x.uses} uses`}</span>
-                  <NeutralBar percentage={`${(x.uses / mostUsed) * 100}%`} />
-                </div>
-                <div className="grid gap-1">
-                  <span className="text-sm text-gray-400">{`${x.props} props`}</span>
-                  <NeutralBar
-                    percentage={`${(x.props / mostUsedProps) * 100}%`}
-                  />
-                </div>
-              </a>
-            </Link>
-          );
-        })}
+      <div>
+        <div className="grid gap-2 mb-16">
+          {data.map((x) => {
+            return (
+              <Link key={x.name} href={`/ikoner/${x.name}`} passHref>
+                <a className="grid gap-12 grid-cols-3 p-3 bg-gray-800 rounded hover:bg-gray-700">
+                  <span className="text-lg ">{`<${x.name}>`}</span>
+                  <div className="grid gap-1">
+                    <span className="text-sm text-gray-400">{`${x.uses} uses`}</span>
+                    <NeutralBar percentage={`${(x.uses / mostUsed) * 100}%`} />
+                  </div>
+                  <div className="grid gap-1">
+                    <span className="text-sm text-gray-400">{`${x.props} props`}</span>
+                    <NeutralBar
+                      percentage={`${(x.props / mostUsedProps) * 100}%`}
+                    />
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+        <h2 className="text-xl mb-4">Not used: {notUsed.length}</h2>
+        <div className="grid gap-2 mb-16">
+          {notUsed.map((x) => {
+            return (
+              <div
+                key={x}
+                className="grid gap-12 grid-cols-3 p-3 bg-gray-800 rounded "
+              >
+                <span className="text-lg">{`<${x}>`}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      icons: ICONS.map((x) => x.name),
+    },
+  };
+}
 
 export default Eksempel;
